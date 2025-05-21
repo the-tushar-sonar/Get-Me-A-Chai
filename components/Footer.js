2b350d9407672dfcbd2828e33ead781d65aa6e55
the-tushar-sonar <mylordtushar@gmail.com>
2025-05-21 14:21:48 +0530

    feat: add Navbar and Footer components, update layout and metadata

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Get Me A Chai - Fund Your Project with Chai</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-black text-white flex items-center justify-between px-4 h16">
+      <div class="logo font-bold text-2xl">GetMeaChai</div>
+      <ul className="flex justify-between items-center gap-4">
+        <li className="ml-4">
+          <a href="/">Home</a>
+        </li>
+        <li className="ml-4">
+          <a href="/about">About</a>
+        </li>
+        <li className="ml-4">
+          <a href="/projects">Projects</a>
+        </li>
+        <li className="ml-4">
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
